8f08867aaf9719ddb069acef015b97c3df83ca16
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-05-08 23:09:34 -0500

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,30 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import axios from 'axios';
 
 const App = () => {
+  
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  
+  useEffect(() => {
+      const fetchItems = async () => {
+        const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+        console.log(result.data)
+        setItems(result.data)
+        setIsLoading(false)
+      }
+
+      fetchItems()
+  }, [])
+
   return (
     <div className="container">
-      Hello
+      <Header />
+      <CharacterGrid />      
     </div>
   );
 }
