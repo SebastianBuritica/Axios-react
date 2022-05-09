850145c3b6aa156a5d104a7011627c3c16c50fed
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-05-08 23:30:43 -0500

    .

--- my-app/src/components/ui/Header.jsx
+++ my-app/src/components/ui/Header.jsx
@@ -3,11 +3,9 @@ import logo from '../../img/logo.png'
 
 const Header = () => {
   return (
-    <>
-        <header className='center'>
-            <img src={logo} alt="" />
-        </header>
-    </>
+    <header className='center'>
+      <img src={logo} alt='' />
+    </header>
   )
 }
 
